5df7d64466f7ecfdf520cd52cc912538ae184493
Ivy Mahncke <imahncke@olin.edu>
2024-04-24 16:53:02 -0400

    Merge

--- Resources/green_forest.tsx
+++ Resources/green_forest.tsx
@@ -1,8 +1,3 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="green_forest" tilewidth="32" tileheight="32" tilecount="390" columns="26">
-<<<<<<< HEAD:Resources/Maps/green_forest.tsx
- <image source="/Resources/Tileset Images/green_forest.png" width="832" height="480"/>
-=======
  <image source="Images/green_forest.png" width="832" height="480"/>
->>>>>>> 69cecfc7d8370e9564fda5e2d59bada2e4f1089c:Resources/green_forest.tsx
-</tileset>
