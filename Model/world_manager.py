8c20084551b368a044ef3def215360eb4a28a779
Ivy Mahncke <imahncke@olin.edu>
2024-04-25 12:38:15 -0400

    Added world_manager and json files
    These files will create the standard world and be
    used as a source for room information

---
+++ Model/world_manager.py
@@ -0,0 +1,21 @@
+"""
+e
+"""
+
+import json
+
+
+class WorldManager:
+    """
+    e
+    """
+
+    def __init__(self):
+        # load in json and create a room for each object
+        # use properties to fill room parameters
+        # may need to update room.py to accept a jsonobject for items,
+        # portals, etc and process those in the room.py file
+        # should possess a dictionary of each room name + room instance
+        pass
+
+    # get room by name function
