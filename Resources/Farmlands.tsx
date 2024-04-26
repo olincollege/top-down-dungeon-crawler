1891b0beba9543ebc74d9de750d9af3dc2b56ba6
Ivy Mahncke <imahncke@olin.edu>
2024-04-26 14:55:31 -0400

    filepaths!!!!!!!!!!

--- Resources/Farmlands.tsx
+++ Resources/Farmlands.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Farmlands" tilewidth="32" tileheight="32" tilecount="1376" columns="32">
- <image source="Resources/Images/Farmlands.png" width="1024" height="1376"/>
+ <image source="Images/Farmlands.png" width="1024" height="1376"/>
 </tileset>
--- Resources/green_dungeon.tsx
+++ Resources/green_dungeon.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Tset1 Test" tilewidth="32" tileheight="32" tilecount="598" columns="26">
- <image source="Resources/Images/green_dungeon.png" width="832" height="736"/>
+ <image source="Images/green_dungeon.png" width="832" height="736"/>
 </tileset>
