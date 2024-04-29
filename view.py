de878b1bbfb57ea1610d95b8f657d0e54b9b920c
Ivy Mahncke <imahncke@olin.edu>
2024-04-29 13:15:36 -0400

    View class is compatible with textboxes now

--- view.py
+++ view.py
@@ -23,20 +23,22 @@ class View:
         Returns a Surface that can be drawn onto the screen.
         """
 
-    def draw(self, player=Player, current_room=Room):
+    def draw(self, text_box, player=Player, current_room=Room):
         """e"""
         # fill with black
         self._screen.fill((0, 0, 0))
-        # draw the current room's lower tiles
-        current_room.get_tile_groups()["Lower"].draw(self._screen)
-        # draw the player
-        player_image = player.sprite_list[player.current_sprite]
-        self._screen.blit(player_image, player.coordinates)
-        # draw the current room's NPCs
-        current_room.get_tile_groups()["NPC"].draw(self._screen)
-        # draw the current room's upper tiles
-        current_room.get_tile_groups()["Upper"].draw(self._screen)
-        # draw textbox
-        # DRAW TEXTBOX CODE
-        # upload the finalized frame image
+        # if no textbox
+        if text_box is None:
+            # draw the current room's lower tiles
+            current_room.get_tile_groups()["Lower"].draw(self._screen)
+            # draw the player
+            player_image = player.sprite_list[player.current_sprite]
+            self._screen.blit(player_image, player.coordinates)
+            # draw the current room's NPCs
+            current_room.get_tile_groups()["NPC"].draw(self._screen)
+            # draw the current room's upper tiles
+            current_room.get_tile_groups()["Upper"].draw(self._screen)
+        # if there is a textbox
+        else:
+            self._screen.blit(text_box)
         pygame.display.flip()
