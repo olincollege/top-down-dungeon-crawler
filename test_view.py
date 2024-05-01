1303bd85a52133e432c23568e2e3f7003dd5a0bc
Mika Gazit <mikagith@gmail.com>
2024-04-30 22:16:48 -0400

    Added pytests for view.py

---
+++ test_view.py
@@ -0,0 +1,11 @@
+"""
+File to test view.py
+
+We will not be testing any of the functions in view.py.
+
+Reasons:
+
+    init: we are not testing initialization functions.
+    draw: this function has no return and deals heavily with
+    displays, making it very difficult to write unit tests for.
+"""
