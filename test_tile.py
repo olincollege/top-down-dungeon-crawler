8090ca534f2197b1580adb1a0bd4afc4271fd974
Charlie Mawn <cmawn@olin.edu>
2024-04-30 22:16:21 -0400

    Made pytests for the tile.py and player.py

---
+++ test_tile.py
@@ -0,0 +1,60 @@
+"""
+Tests the methods in the tile.py file
+"""
+
+import pygame
+import pytest
+from Model.player import Player
+from Model.tile import NPC, Item
+from controller import TopDownController
+from view import View
+
+
+pygame.init()
+view = View()
+
+
+test_cont = TopDownController()
+test_play = Player(
+    [
+        "Resources/Player Images/red_up_32.png",
+        "Resources/Player Images/red_right_32.png",
+        "Resources/Player Images/red_down_32.png",
+        "Resources/Player Images/red_left_32.png",
+    ],
+    0,
+    "Coco",
+    (0, 0),
+    test_cont.current_room,
+)
+
+test_item = Item(
+    "silver_coin",
+    (0,0),
+    pygame.sprite.Group(),
+    pygame.image.load("Resources/npc_images/npc_male.png"),
+)
+
+test_npc = NPC(
+    ["before","current","after"],
+    "silver_coin", 
+    pygame.image.load("Resources/npc_images/npc_male.png"),
+    pygame.sprite.Group(),
+    "Felix",
+    (0,0),
+)
+
+test_cases = [
+    (test_npc, test_play, test_item)
+]
+
+@pytest.mark.parametrize(
+        "npc, player, item", test_cases
+)
+def test_det_voice(npc, player, item):
+    """
+    Tests the det_voice method in the NPC class of tile.py
+    """
+    player.pick_up(item)
+    voice_line = npc.det_voice(player)
+    assert voice_line == "before"
