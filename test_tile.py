45d2473613b73c4aa3fb3b21363daa07c53e24c4
Charlie Mawn <cmawn@olin.edu>
2024-04-30 22:21:34 -0400

    Unit-testing

--- test_tile.py
+++ test_tile.py
@@ -30,30 +30,31 @@ test_play = Player(
 
 test_item = Item(
     "silver_coin",
-    (0,0),
+    (0, 0),
     pygame.sprite.Group(),
     pygame.image.load("Resources/npc_images/npc_male.png"),
 )
 
 test_npc = NPC(
-    ["before","current","after"],
-    "silver_coin", 
+    ["before", "current", "after"],
+    "silver_coin",
     pygame.image.load("Resources/npc_images/npc_male.png"),
     pygame.sprite.Group(),
     "Felix",
-    (0,0),
+    (0, 0),
 )
 
-test_cases = [
-    (test_npc, test_play, test_item)
-]
+test_cases = [(test_npc, test_play, test_item)]
 
-@pytest.mark.parametrize(
-        "npc, player, item", test_cases
-)
+
+@pytest.mark.parametrize("npc, player, item", test_cases)
 def test_det_voice(npc, player, item):
     """
     Tests the det_voice method in the NPC class of tile.py
+
+    Only testing the 'before' test because to update the state of the 
+    npc the player has to interact with it in a way that can not be done
+    in a unit test
     """
     player.pick_up(item)
     voice_line = npc.det_voice(player)
